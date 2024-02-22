58eb14f9b91aec33c6d45fe82b4d6a9cd30cce4a
tulxoro <tulxoro@hotmail.com>
2024-02-21 22:53:23 -0800

    reverted mock-up changes

---
+++ application/frontend/src/lib/components/main/Footer.svelte
@@ -0,0 +1,3 @@
+<footer class="fixed bottom-0 w-full text-center">
+    CSC648 Team 5 © 2024
+</footer>
---
+++ application/frontend/src/lib/components/main/Header.svelte
@@ -0,0 +1,7 @@
+<header class="w-full text-center py-4">
+    <h2 class="text-red-500">
+      SFSU Software Engineering Project CSC 648-848, Spring 2024. For
+      Demonstration Only
+    </h2>
+    <h1>CSC648 Project</h1>
+  </header>
