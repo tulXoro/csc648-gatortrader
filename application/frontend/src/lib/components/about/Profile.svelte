edc9b44b6b0e6d2eca0d6a78bce01adb2b0678b8
tulxoro <tulxoro@hotmail.com>
2024-02-22 00:21:17 -0800

    made temp profile and basic profile template

---
+++ application/frontend/src/lib/components/about/Profile.svelte
@@ -0,0 +1,22 @@
+<script>
+    export let name = "";
+    export let photo = "";
+    export let bio = "";
+    export let role = "";
+</script>
+
+<main>
+<div class="container center">
+    <h1>About Us</h1>
+    <div class="row">
+        <div class="col-md-6">
+            <img src={photo} alt={name} class="img-fluid" />
+        </div>
+        <div class="col-md-6">
+            <h2>{name}</h2>
+            <h3>{role}</h3>
+            <p>{bio}</p>
+        </div>
+    </div>
+</div>
+</main>
