f1a32e7bf4b9acaaf3f3005da16d0e81bddb93fb
jwong51 <jwong51@mail.sfsu.edu>
2024-02-22 22:43:14 -0800

    create my feature about

--- application/frontend/src/routes/about/+page.svelte
+++ application/frontend/src/routes/about/+page.svelte
@@ -4,6 +4,6 @@
     <li><a href="#">team member 2</a></li>
     <li><a href="#">team member 3</a></li>
     <li><a href="#">team member 4</a></li>
-    <li><a href="#">team member 5</a></li>
+    <li><a href="#">Jayden Wong</a></li>
     <li><a href="#">team member 6</a></li>
 </ul>
---
+++ application/frontend/src/routes/about/jayden/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+		import Nav from '$lib/components/main/Nav.svelte';
+		import Footer from '$lib/components/main/Footer.svelte';
+		import Header from '$lib/components/main/Header.svelte';
+
+		import Profile from '$lib/components/about/Profile.svelte';
+</script>
+
+<!-- Commented out because nav obstructs profile right now -->
+<!-- <Nav /> -->
+
+<Header />
+
+<Profile name="Jayden Wong"
+		photo="https://via.placeholder.com/300"
+		bio="This is a bio"
+		role="Back End Lead"
+
+		github="https://github.com/jwong51"
+		linkedin="https://linkedin.com"
+		email="jwong51@mail.sfsu.edu"
+/>
+
+<Footer />
