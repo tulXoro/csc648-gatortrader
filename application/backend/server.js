c732cac272a97766223c2e92173b776d2573497a
Tim <tulxoro@hotmail.com>
2024-02-24 00:01:11 -0800

    Added express server to the project

---
+++ application/backend/server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import {handler} from '../frontend/build/handler.js'
+
+const app = express();
+const PORT = 3001;
+
+app.get('/backtest', (req, res) => {
+  res.send('Hello World');
+});
+
+app.use(handler);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
