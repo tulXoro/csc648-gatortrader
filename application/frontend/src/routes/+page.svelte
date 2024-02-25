8f006c4403a0239a62e808e9e7ee47c71ccacd0a
Tim <tulxoro@hotmail.com>
2024-02-25 14:56:34 -0800

    update for m0

--- application/frontend/src/routes/+page.svelte
+++ application/frontend/src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<script>
+<!-- <script>
 	import Nav from "$lib/components/main/Nav.svelte";
 	import Header from "$lib/components/main/Header.svelte";
 	import Footer from "$lib/components/main/Footer.svelte";
@@ -41,4 +41,23 @@
 	</div>
 </div>
 
-<Footer/>
+<Footer/> -->
+
+<script>
+    import Header from '$lib/components/main/Header.svelte';
+    import Footer from '$lib/components/main/Footer.svelte';
+</script>
+
+<Header />
+
+
+<ul>
+    <li><a href="./about/luis">Luis Angeles</a></li>
+    <li><a href="./about/cole">Cole Douglas</a></li>
+    <li><a href="./about/Singh">Parampal Singh</a></li>
+    <li><a href="./about/jayden">Jayden Wong</a></li>
+    <li><a href="./about/tim">Tim Lor</a></li>
+    <li><a href="./about/Haley">Haley Park</a></li>
+</ul>
+
+<Footer />
