a8e61a5baf986f1ad0b5407192870c8ccbaaeae7
junghyeon <j01h02@gmail.com>
2024-02-24 19:55:17 -0800

    updated my profile page

--- application/frontend/src/routes/about/+page.svelte
+++ application/frontend/src/routes/about/+page.svelte
@@ -5,5 +5,5 @@
     <li><a href="#">team member 3</a></li>
     <li><a href="#">team member 4</a></li>
     <li><a href="#">team member 5</a></li>
-    <li><a href="#">team member 6</a></li>
+    <li><a href="#">Haley Park</a></li>
 </ul>
