5d44ab537fa4b6540a2dc1e4ccaa225e491f45a2
Tim <tulxoro@hotmail.com>
2024-03-29 19:30:06 -0700

    Maybe allows filtering by category

--- application/backend/routes/getPosts.js
+++ application/backend/routes/getPosts.js
@@ -4,9 +4,22 @@ import db from '../conf/database.js';
 const router = express.Router();
 
 router.get('/', async (req, res) => {
+
+
     try {
-        const results = await db.query('SELECT * FROM t_product_post');
-        res.json(results);
+        // when we receive a request to query by category
+        if (req.query.category) {
+            const [rows] = await db.query(`
+                SELECT * FROM posts
+                WHERE category = ?
+            `, [req.query.category]);
+            res.json(rows);
+        } else {
+            const [rows] = await db.query(`
+                SELECT * FROM posts
+            `);
+            res.json(rows);
+        }
 
         // NOTE: Does not send image data. Client will need to make a separate request to get image data.
     } catch (err) {
