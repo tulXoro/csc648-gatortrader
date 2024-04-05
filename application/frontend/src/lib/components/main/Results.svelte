65d0aa31fc4e67a3356db22718664a859e8cb08a
Tim <tulxoro@hotmail.com>
2024-04-04 17:22:08 -0700

    fixed stagnant pagination

--- application/frontend/src/lib/components/main/Results.svelte
+++ application/frontend/src/lib/components/main/Results.svelte
@@ -1,59 +1,32 @@
 <script lang="ts">
   import { onMount } from "svelte";
+  import { derived } from "svelte/store";
 
-  let posts: any[] = [];
-  let helper = { start: 1, end: 10, total: 0 };
+  import { posts } from "../../store.js";
+
   let searchInput = "";
 
-  // OPTIMIZE THIS FUNCTION SO THAT IT DOESNT REQUIRE FETCHING DATA
-  async function fetchProductData() {
-    try {
-      const urlParams = new URLSearchParams(window.location.search);
-      searchInput = urlParams.get("search") || ""; // Get search query from URL
+  const helper = derived(posts, $posts => ({
+    start: Math.min($posts.length, 1),
+    end: Math.min($posts.length, 10),
+    total: $posts.length,
+  }));
 
-      // Fetch data from server
-      const response = await fetch("/getPosts");
-      if (response.ok) {
-        posts = await response.json();
-        // Filter posts based on the search query and status
-        const NumResults = posts.filter(
-          (post) =>
-            (!searchInput ||
-              post.item_name
-                .toLowerCase()
-                .includes(searchInput.toLowerCase())) &&
-            post.status === "APPROVED"
-        );
-
-        // Query Search Results
-        helper.total = NumResults.length;
-        helper.start = 1;
-        helper.end = Math.min(NumResults.length, 10);
-      } else {
-        console.error("Failed to fetch product data");
-      }
-    } catch (error) {
-      console.error("Error fetching product data:", error);
-    }
-  }
-
-  // Call fetchProductData function when the component mounts
-  onMount(fetchProductData);
 </script>
 
 <div class="flex flex-col margin-left gap-2">
   <div class="text-sm text-gray-700 dark:text-gray-400">
     <!-- Display pagination information -->
     <span class="font-semibold text-gray-900 dark:text-white">
-      {helper.start}
+      {$helper.start}
     </span>
     {" - "}
     <span class="font-semibold text-gray-900 dark:text-white">
-      {helper.end}
+      {$helper.end}
     </span>
     {" of over "}
     <span class="font-semibold text-gray-900 dark:text-white">
-      {helper.total}
+      {$helper.total}
     </span>
     {" results "}
     <!-- Display search query if available -->
