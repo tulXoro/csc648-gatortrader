b44c13b0dca38f6809e29c97e64d6eced9a9c7de
langeles014 <angeles.bryant@yahoo.com>
2024-04-07 21:23:06 -0700

    rerouted about us pages

--- application/frontend/src/routes/about/Singh/+page.svelte
+++ application/frontend/src/routes/about/Singh/+page.svelte
@@ -1,24 +1,14 @@
 <script>
-  import Nav from "$lib/components/main/Nav.svelte";
-  import Footer from "$lib/components/main/Footer.svelte";
-  import Header from "$lib/components/main/Header.svelte";
-
-  import Profile from "../../Profile.svelte";
+  import img from "$lib/assets/image.jpg";
+  import Profile from "$lib/components/main/Profile.svelte";
 </script>
-
-<!-- Commented out because nav obstructs profile right now -->
-<!-- <Nav /> -->
-
-<Header />
-<Nav />
 
 <Profile
   name="Parampal Singh"
+  photo={img}
   bio="This is a bio"
   role="Github Master"
   github="https://github.com/singh-paul"
   linkedin="https://www.linkedin.com/in/parampal-singh-1a2978172/"
   email="psingh17@sfsu.edu"
 />
-
-<Footer />
--- application/frontend/src/routes/about/jayden/+page.svelte
+++ application/frontend/src/routes/about/jayden/+page.svelte
@@ -1,24 +1,14 @@
 <script>
-  import Nav from "$lib/components/main/Nav.svelte";
-  import Footer from "$lib/components/main/Footer.svelte";
-  import Header from "$lib/components/main/Header.svelte";
-
-  import Profile from "../../Profile.svelte";
+  import img from "$lib/assets/image.jpg";
+  import Profile from "$lib/components/main/Profile.svelte";
 </script>
-
-<!-- Commented out because nav obstructs profile right now -->
-<!-- <Nav /> -->
-
-<Header />
-<Nav />
 
 <Profile
   name="Jayden Wong"
+  photo={img}
   bio="This is a bio"
   role="Back End Lead"
   github="https://github.com/jwong51"
   linkedin="https://linkedin.com"
   email="jwong51@mail.sfsu.edu"
 />
-
-<Footer />
