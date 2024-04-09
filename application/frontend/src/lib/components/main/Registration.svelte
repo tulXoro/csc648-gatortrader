0e498ae180a4965b2cafa0e66ca28b2410c50a9d
langeles014 <angeles.bryant@yahoo.com>
2024-04-08 17:12:32 -0700

    registration.svelte w/ valdation middleware

--- application/frontend/src/lib/components/main/Registration.svelte
+++ application/frontend/src/lib/components/main/Registration.svelte
@@ -1,68 +1,177 @@
 <script>
-  import { Input, Label, Helper, Button, Checkbox, A } from "flowbite-svelte";
+  import {
+    Input,
+    Label,
+    Button,
+    Checkbox,
+    A,
+    ButtonGroup,
+    InputAddon,
+    ToolbarButton,
+  } from "flowbite-svelte";
+  import { Popover } from "flowbite-svelte";
+  import { EyeOutline, EyeSlashOutline } from "flowbite-svelte-icons";
+
+  // email criteria
+  let email = "";
+  function validateEmail() {
+    if (!email.trim()) return false; // if empty
+    return email.trim().toLowerCase().endsWith("@sfsu.edu");
+  }
+
+  // Function to handle form submission
+  function handleSubmit() {
+    checkPasswordCriteria(); // Check password criteria
+    if (!validateEmail()) {
+      alert("Please enter a valid email ending with '@sfsu.edu'.");
+    } else if (!Object.values(criteria).every((c) => c === true)) {
+      alert("Please make sure your password meets all criteria.");
+    } else {
+      // Proceed with form submission
+      console.log("Email and password are valid:", email, password);
+      // Add your form submission logic here
+    }
+  }
+
+  // show password
+  let showPW = false;
+
+  // password criteria
+  let password = "";
+  let criteria = {
+    length: false,
+    number: false,
+    uppercase: false,
+    lowercase: false,
+    specialChar: false,
+  };
+
+  // real-time validator
+  function checkPasswordCriteria() {
+    criteria.length = password.length >= 8;
+    criteria.number = /\d/.test(password);
+    criteria.uppercase = /[A-Z]/.test(password);
+    criteria.lowercase = /[a-z]/.test(password);
+    criteria.specialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+  }
 </script>
 
-<form>
-  <div class="grid gap-6 mb-6 md:grid-cols-2">
-    <div>
-      <Label for="first_name" class="mb-2">First name</Label>
+<form method="POST" action="?/create">
+  <div class="form-container">
+    <div class="mb-6">
+      <Label for="username" class="mb-2">Username</Label>
       <Input type="text" id="first_name" placeholder="John" required />
     </div>
-    <div>
-      <Label for="last_name" class="mb-2">Last name</Label>
-      <Input type="text" id="last_name" placeholder="Doe" required />
-    </div>
-    <div>
-      <Label for="company" class="mb-2">Company</Label>
-      <Input type="text" id="company" placeholder="Flowbite" required />
-    </div>
-    <div>
-      <Label for="phone" class="mb-2">Phone number</Label>
+    <div class="mb-6">
+      <Label for="email" class="mb-2">Email</Label>
       <Input
-        type="tel"
-        id="phone"
-        placeholder="123-45-678"
-        pattern={"[0-9]{3}-[0-9]{2}-[0-9]{3}"}
+        type="email"
+        id="email"
+        placeholder="@sfsu.edu"
+        bind:value={email}
         required
       />
     </div>
-    <div>
-      <Label for="website" class="mb-2">Website URL</Label>
-      <Input type="url" id="website" placeholder="flowbite.com" required />
+
+    <div class="mb-6">
+      <Label for="password" class="mb-2">Password</Label>
+      <ButtonGroup class="w-full flex justify-between">
+        <Input
+          id="password"
+          type={showPW ? "text" : "password"}
+          placeholder="••••••••"
+          bind:value={password}
+          on:input={checkPasswordCriteria}
+        />
+        <InputAddon>
+          <Button on:click={() => (showPW = !showPW)}>
+            {#if showPW}
+              <EyeOutline class="w-5 h-5 eye-icon" />
+            {:else}
+              <EyeSlashOutline class="w-5 h-5" />
+            {/if}
+          </Button>
+        </InputAddon>
+      </ButtonGroup>
     </div>
-    <div>
-      <Label for="visitors" class="mb-2">Unique visitors (per month)</Label>
-      <Input type="number" id="visitors" placeholder="" required />
+
+    <Popover class="text-sm" triggeredBy="#password" placement="bottom">
+      <div class="grid grid-cols-1 gap-2">
+        <div class="flex items-center">
+          <div
+            class="{criteria.length
+              ? 'bg-green-400'
+              : 'bg-gray-200 dark:bg-gray-600'} rounded-full h-3 w-3 flex items-center justify-center"
+          >
+            {criteria.length ? "✓" : ""}
+          </div>
+          <span class="ml-2"> Must have at least 8 characters </span>
+        </div>
+        <div class="flex items-center">
+          <div
+            class="{criteria.number
+              ? 'bg-green-400'
+              : 'bg-gray-200 dark:bg-gray-600'} rounded-full h-3 w-3 flex items-center justify-center"
+          >
+            {criteria.number ? "✓" : ""}
+          </div>
+          <span class="ml-2"> Have one number </span>
+        </div>
+        <div class="flex items-center">
+          <div
+            class="{criteria.uppercase
+              ? 'bg-green-400'
+              : 'bg-gray-200 dark:bg-gray-600'} rounded-full h-3 w-3 flex items-center justify-center"
+          >
+            {criteria.uppercase ? "✓" : ""}
+          </div>
+          <span class="ml-2"> Have one uppercase character </span>
+        </div>
+
+        <div class="flex items-center">
+          <div
+            class="{criteria.lowercase
+              ? 'bg-green-400'
+              : 'bg-gray-200 dark:bg-gray-600'} rounded-full h-3 w-3 flex items-center justify-center"
+          >
+            {criteria.lowercase ? "✓" : ""}
+          </div>
+          <span class="ml-2"> Have one lowercase character </span>
+        </div>
+
+        <div class="flex items-center">
+          <div
+            class="{criteria.specialChar
+              ? 'bg-green-400'
+              : 'bg-gray-200 dark:bg-gray-600'} rounded-full h-3 w-3 flex items-center justify-center"
+          >
+            {criteria.specialChar ? "✓" : ""}
+          </div>
+          <span class="ml-2"> Have one special character </span>
+        </div>
+      </div>
+    </Popover>
+
+    <Checkbox class="mb-6 space-x-1 rtl:space-x-reverse" required>
+      I agree with the<A
+        href="#"
+        class="text-primary-700 dark:text-primary-600 hover:underline"
+        >terms and conditions</A
+      >.
+    </Checkbox>
+    <div class="flex justify-center">
+      <Button class="" type="button" on:click={handleSubmit}>Submit</Button>
     </div>
   </div>
-  <div class="mb-6">
-    <Label for="email" class="mb-2">Email address</Label>
-    <Input
-      type="email"
-      id="email"
-      placeholder="john.doe@company.com"
-      required
-    />
-  </div>
-  <div class="mb-6">
-    <Label for="password" class="mb-2">Password</Label>
-    <Input type="password" id="password" placeholder="•••••••••" required />
-  </div>
-  <div class="mb-6">
-    <Label for="confirm_password" class="mb-2">Confirm password</Label>
-    <Input
-      type="password"
-      id="confirm_password"
-      placeholder="•••••••••"
-      required
-    />
-  </div>
-  <Checkbox class="mb-6 space-x-1 rtl:space-x-reverse" required>
-    I agree with the <A
-      href="/"
-      class="text-primary-700 dark:text-primary-600 hover:underline"
-      >terms and conditions</A
-    >.
-  </Checkbox>
-  <Button type="submit">Submit</Button>
 </form>
+
+<style>
+  .form-container {
+    max-width: 400px;
+    margin: 0 auto;
+    padding: 20px;
+    border: 1px solid #ccc;
+    border-radius: 8px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  }
+</style>
