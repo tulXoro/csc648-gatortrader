903d356d8a54bbbf960befcc59c33d6bcbd82279
jwong51 <jwong51@mail.sfsu.edu>
2024-04-22 22:08:16 -0700

    return textbook data when there is a category

--- application/backend/routes/getPosts.js
+++ application/backend/routes/getPosts.js
@@ -7,15 +7,15 @@ router.get("/", async (req, res) => {
   try {
     const { category, search } = req.query;
     if (category && search) {
-      const sql = `SELECT * FROM t_product_post WHERE
-                      category_id = ? AND
-                      (item_name LIKE '%${search}%' OR item_description LIKE '%${search}%') AND status = 'APPROVED'`;
+      const sql = `SELECT pp.*, t.couse_number, t.professor FROM t_product_post pp left outer join t_textbook t on pp.post_id = t.post_id WHERE
+                      pp.category_id = ? AND
+                      (pp.item_name LIKE '%${search}%' OR pp.item_description LIKE '%${search}%') AND pp.status = 'APPROVED'`;
       const [rows] = await db.query(sql, [category]);
       res.json(rows);
     }
     // when we receive a request to query by category
     else if (category) {
-      const [rows] = await db.query("SELECT * FROM t_product_post WHERE category_id = ? AND status = 'APPROVED'", [category]);
+      const [rows] = await db.query("SELECT pp.*, t.couse_number, t.professor FROM t_product_post pp left outer join t_textbook t on pp.post_id = t.post_id WHERE pp.category_id = 1 AND pp.status = 'APPROVED'", [category]);
       res.json(rows);
     }
     else if (search) {
