48e5d814f698885bfbe60c91077164a20e717599
jwong51 <jwong51@mail.sfsu.edu>
2024-05-03 16:04:27 -0700

    added password encryption tp resgisterUser and added getCategories api

--- application/backend/routes/registerUser.js
+++ application/backend/routes/registerUser.js
@@ -1,5 +1,7 @@
 import express from "express";
 import db from "../conf/database.js";
+import bcrypt from "bcrypt";
+
 
 const router = express.Router();
 
@@ -8,8 +10,18 @@ router.post("/", async (req, res) => {
     // Extract user data from the request body
     // console.log(req.body);
     const { userName, password, lastName, firstName, email } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    console.log(`password: ${password}, hashedPassword: ${hashedPassword}`);
+    bcrypt.compare(password, hashedPassword, (err, result) => {
+      if (err) {
+        console.log(`error in comparing password: ${err}`);
+      }
+      if (result) {
+        console.log(`successful in comparing original and hashed passwords: ${result}`);
+      }
+    })
     const sql = "INSERT INTO t_user (user_name, password, last_name, first_name, email) values (?, ?, ?, ?, ?)";
-    const result = await db.query(sql, [userName, password, lastName, firstName, email]);
+    const result = await db.query(sql, [userName, hashedPassword, lastName, firstName, email]);
     res.status(201).json({ message: `User ${userName} is registered successfully...` });
     console.log(result);
   } catch (err) {
