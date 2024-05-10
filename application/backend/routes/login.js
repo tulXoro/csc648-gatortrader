5438d6941a4e65e4893669ed798047dfa8bc72a4
jwong51 <jwong51@mail.sfsu.edu>
2024-05-09 21:11:10 -0700

    Added session management and clean up code

--- application/backend/routes/login.js
+++ application/backend/routes/login.js
@@ -11,9 +11,9 @@ router.post("/", async (req, res) => {
         // Check userName
         const sqlUserName = "SELECT * FROM t_user WHERE user_name = ?";
         const [users] = await db.query(sqlUserName, [userName]);
-        users.forEach(user => {
-            console.log(user);
-        })
+        // users.forEach(user => {
+        //     console.log(user);
+        // })
 
         if (users.length === 0) {
             return res.status(401).json({ message: 'Invalid username or password' });
@@ -22,7 +22,7 @@ router.post("/", async (req, res) => {
         // Check password
         const loginUser = users[0];
 
-        console.log("loginUser: ", loginUser);
+        // console.log("loginUser: ", loginUser);
 
         const { password: hashedPassword, userId } = loginUser;
         const isPasswordMatch = bcrypt.compareSync(password, hashedPassword);
@@ -30,7 +30,8 @@ router.post("/", async (req, res) => {
             console.log("Passwords do not match");
             return res.status(401).json({ message: 'Invalid username or password' });
         }
-        // req.session.user = { id: userId, username: userName };
+        // console.log("session:", req.session);
+        req.session.user = { id: userId, username: userName };
         res.status(200).json({ message: `User ${userName} is logined successfully...` });
 
         } catch (err) {
--- application/backend/routes/registerUser.js
+++ application/backend/routes/registerUser.js
@@ -21,19 +21,19 @@ router.post("/", async (req, res) => {
     // console.log(req.body);
     const { userName, password, lastName, firstName, email } = req.body;
     const hashedPassword = await bcrypt.hash(password, 10);
-    console.log(`password: ${password}, hashedPassword: ${hashedPassword}`);
-    bcrypt.compare(password, hashedPassword, (err, result) => {
-      if (err) {
-        console.log(`error in comparing password: ${err}`);
-      }
-      if (result) {
-        console.log(`successful in comparing original and hashed passwords: ${result}`);
-      }
-    })
+    // console.log(`password: ${password}, hashedPassword: ${hashedPassword}`);
+    // bcrypt.compare(password, hashedPassword, (err, result) => {
+    //   if (err) {
+    //     console.log(`error in comparing password: ${err}`);
+    //   }
+    //   if (result) {
+    //     console.log(`successful in comparing original and hashed passwords: ${result}`);
+    //   }
+    // })
     const sql = "INSERT INTO t_user (user_name, password, last_name, first_name, email) values (?, ?, ?, ?, ?)";
     const result = await db.query(sql, [userName, hashedPassword, lastName, firstName, email]);
     res.status(201).json({ message: `User ${userName} is registered successfully...` });
-    console.log(result);
+    // console.log(result);
   } catch (err) {
     res.status(500).send("Error in registering user..." + err);
   }
