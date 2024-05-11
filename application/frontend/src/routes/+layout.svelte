5d908700532bf3ef9014e549a1ac009d3f8fbe39
Parampal singh <parampalsingh@parampals-mbp.lan>
2024-05-11 12:16:21 -0700

    Add Google Analytics tracking code

--- application/frontend/src/routes/+layout.svelte
+++ application/frontend/src/routes/+layout.svelte
@@ -1,5 +1,24 @@
 <script>
     import "../app.css";
-</script>
-
-<slot />
+    import { onMount } from 'svelte';
+  
+    // Adding Google Analytics tracking code on mount
+    onMount(() => {
+      const script = document.createElement('script');
+      script.src = 'https://www.googletagmanager.com/gtag/js?id=G-ZRRPBNQ55F';
+      script.async = true;
+      document.head.appendChild(script);
+  
+      const script2 = document.createElement('script');
+      script2.textContent = `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-ZRRPBNQ55F');
+      `;
+      document.head.appendChild(script2);
+    });
+  </script>
+  
+  <slot />
+  
