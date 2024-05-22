a0ac77e425d14f2942ed51ec980835b74cc1fab4
Tim <tulxoro@hotmail.com>
2024-05-22 03:46:17 -0700

    brought back nav arrows

--- application/frontend/src/routes/browse/+page.svelte
+++ application/frontend/src/routes/browse/+page.svelte
@@ -188,15 +188,15 @@
 
 <!-- Pagination -->
 
-<!-- <div class="flex flex-col items-center justify-center gap-2 mt-2">
-  <div class="text-sm text-gray-700 dark:text-gray-400">
+<div class="flex flex-col items-center justify-center gap-2 mt-2">
+  <!-- <div class="text-sm text-gray-700 dark:text-gray-400">
     Showing <span class="font-semibold text-gray-900 dark:text-white">{helper.start}</span>
     to
     <span class="font-semibold text-gray-900 dark:text-white">{helper.end}</span>
     of
     <span class="font-semibold text-gray-900 dark:text-white">{helper.total}</span>
     Entries
-  </div>
+  </div> -->
 
 
 <div class="flex flex-col items-center justify-center gap-2">
@@ -207,5 +207,5 @@
   </div>
 </div>
 
-</div> -->
+</div>
 <!-- end pagination -->
